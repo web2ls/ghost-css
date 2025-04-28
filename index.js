31d9e770f72974c3b386a5abbda722a94d59760f
web2ls <web2ls@yandex.ru>
2025-04-28 23:20:03 +0400

    add 3 party libs

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ import { read, walk } from "files";
 const results = [];
 
 const readmes = await walk("test")
+.filter(/[.tsx | .pcss]$/);
 console.log(readmes);
+
+const file = await read(readmes[0]);
+console.log(file);
